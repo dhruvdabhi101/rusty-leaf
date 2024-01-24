5b6c6ad7371aeb766be9175c94b826dec91b0140
Dhruv <dhruvdabhi101@gmail.com>
2024-01-25 01:23:42 +0530

    first arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+
+/// simple program to greet
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// name of the person
+    #[clap(short, long)]
+    name: String,
+    /// age of the person
+    #[clap(short, long)]
+    age: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    for _ in 0..args.age {
+        println!("Hello, {}!", args.name);
+    }
 }
