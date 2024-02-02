0c4479e7f57461c5d1e7fbf85f74c5e4067ab8a7
Dhruv <dhruvdabhi101@gmail.com>
2024-02-02 09:49:05 +0530

    feat: deploy with CMD

--- src/service.rs
+++ src/service.rs
@@ -1,8 +1,18 @@
 use std::{collections::HashMap, str::FromStr};
 
+use colored::{Color, Colorize};
 use reqwest::header::AUTHORIZATION;
+use serde::{Serialize, Deserialize};
 
 use crate::runner::get_token;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PageRequest {
+    title: String,
+    content: String,
+    slug: String,
+    published: bool,
+}
 
 /// Login Request to the server
 pub fn login_request(name: String, password: String) -> String {
@@ -26,21 +36,37 @@ pub fn create_page(title: String, content: String, slug: String, published: bool
     println!("{}", token);
     // set authorization header
     let url = "http://127.0.0.1:8000/pages/create-page";
-    let mut map = HashMap::new();
-    map.insert("title", title);
-    map.insert("content", content);
-    map.insert("slug", slug);
-    map.insert("published", published.to_string());
-    let res = client.post(url).json(&map).bearer_auth(token).send().unwrap();
+    // let mut map = HashMap::new();
+    // map.insert("title", title);
+    // map.insert("content", content);
+    // map.insert("slug", slug);
+    // map.insert("published", published);
+    let map = PageRequest {
+        title,
+        content,
+        slug,
+        published,
+    };
+    let temp = reqwest::header::HeaderValue::from_bytes(token.as_bytes()).unwrap();
+    println!("{:?}", temp);
+    let res = client
+        .post(url)
+        .json(&map)
+        .header(
+            reqwest::header::AUTHORIZATION,
+            reqwest::header::HeaderValue::from_str(&token).unwrap(),
+
+        )
+        .send()
+        .unwrap();
     if res.status() != 200 {
         panic!("Create Page failed");
+    } else {
+        println!("{}","Page created successfully".color(Color::Green));
     }
-    let page = res.json::<HashMap<String, String>>().unwrap();
-    println!("{:?}", page);
 }
 
 fn make_bearer_token(token: String) -> String {
     let token = format!("Bearer {}", token);
     token
 }
-
